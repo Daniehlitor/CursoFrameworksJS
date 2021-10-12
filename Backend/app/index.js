cd56ff62323413d96820b4de616de66df4c05ab4
Daniel Jerez <Daniehlitor@gmail.com>
2021-10-11 23:15:02 -0500

    Nada

--- Backend/app/index.js
+++ Backend/app/index.js
@@ -6,7 +6,6 @@ var port = 3900;
 
 mongoose.Promise = global.Promise;
 
-
 mongoose.connect('mongodb://localhost:27017/api_rest_blog',{ useNewUrlParser:true, useUnifiedTopology: true }).then(() => {
     //Creacion del server
     app.listen(port, () => {
